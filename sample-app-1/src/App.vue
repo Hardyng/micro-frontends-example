99e20e804128be4100d3246bedb58095167a5014
Adam Kalinowski <adam.kalinowski.1997@gmail.com>
2019-09-11 21:45:00 +0200

    Init

---
+++ sample-app-1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Sample app here!</h1>
+    <h3>And here is some of sample app routing</h3>
+    <div id="nav">
+      <router-link to="/">Feature 1</router-link> |
+      <router-link to="/feature">Feature 2</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ sample-app-1/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vueCustomElement from 'vue-custom-element'
+
+Vue.config.productionTip = false
+
+Vue.use(vueCustomElement)
+
+App.store = store
+App.router = router
+Vue.customElement('sample-app-1', App)
